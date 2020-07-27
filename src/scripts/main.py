71cd4f89e2ab6e2e625430343d2ba37be57ee69b
7rick03ligh7 <7rick03ligh7@gmail.com>
2020-07-27 20:30:42 +0900

    [EDIT] init main

--- src/scripts/main.py
+++ src/scripts/main.py
@@ -4,26 +4,49 @@ import random
 import shutil
 
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader
-
-import torchvision
-import torchvision.transforms as transforms
-
 import pytorch_lightning as pl
 from pytorch_lightning import loggers as pl_loggers
 from pytorch_lightning import Trainer
-from pytorch_lightning.metrics.classification import confusion_matrix
 from pytorch_lightning.callbacks import ModelCheckpoint
 
-LOGDIR = 'logs'
-os.makedirs('./'+LOGDIR, exist_ok=True)
-torchvision.datasets.CIFAR10('./data', train=True, download=True)
-torchvision.datasets.CIFAR10('./data', train=False, download=True)
+from src.models.vgg_Like import VGGLike
+import multiprocessing as mp
+import argparse
+import json
+from src.utils.generates import *
+
+
+def worker(pid, queue, model_params):
+    model = VGGLike(model_params)
+
+def main(models_params):
+    
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--logdir', type=str, required=True)
+    parser.add_argument('--params_file', type=str, required=True)
+    parser.add_argument('--epochs', type=int, required=True)
+    parser.add_argument('--seed', type=int, default=42, required=False)
+    arguments = parser.parse_args()
+
+    with open(arguments.params_file, 'r') as f:
+        models_params = json.load(f)
+
+    for model_params in models_params:
+        model_params['classes'] = [
+            'airplane',
+            'automobile',
+            'bird',
+            'cat',
+            'deer',
+            'dog',
+            'frog',
+            'horse',
+            'ship',
+            'truck'
+        ]
+        logname, logpath = generate_logname(model_params)
+        model_params['logpath'] = logpath
+        model_params['seed'] = arguments.seed
+
