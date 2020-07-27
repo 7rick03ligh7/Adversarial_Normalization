caeeb5376b6062cc8537b63e02fd92f8be5b1766
7rick03ligh7 <7rick03ligh7@gmail.com>
2020-07-27 22:06:00 +0900

    [ADD/EDIT] add multiprocessing [EDIT] main run bash

--- src/scripts/main.py
+++ src/scripts/main.py
@@ -14,15 +14,61 @@ import multiprocessing as mp
 import argparse
 import json
 from src.utils.generates import *
+from tqdm import tqdm
 
 
 def worker(pid, queue, model_params):
+    if os.path.exists(model_params['logpath']):
+        shutil.rmtree(model_params['logpath'])
+    os.makedirs(model_params['logpath'], exist_ok=True)
+
     model = VGGLike(model_params)
+    tb_logger = pl_loggers.TensorBoardLogger(
+        model_params['logdir'],
+        name=model_params['logname'],
+        version=''
+        )
+    os.makedirs(model_params['logpath']+'/checkpoints/best/', exist_ok=True)
+    checkpoint_callback = ModelCheckpoint(
+        filepath=model_params['logpath']+'/checkpoints/best/',
+        save_top_k=1,
+        verbose=False,
+        monitor='val_acc',
+        mode='max',
+        prefix=''
+    )
+    trainer = Trainer(
+        logger=tb_logger,
+        checkpoint_callback=checkpoint_callback,
+        max_epochs=100,
+        num_sanity_val_steps=0,
+        weights_summary=None,
+        progress_bar_refresh_rate=0,
+        track_grad_norm=1,
+        gpus=[os.environ['CUDA_VISIBLE_DEVICES']]
+        )
+    trainer.fit(model)
 
 def main(models_params):
-    
+    progress_bars = []
+    processes = []
+    progress_queue = mp.Queue()
+    for i, model_params in enumerate(models_params):
+        progress_bars.append(tqdm(total=model_params['epochs']))
+        proc = mp.Process(target=worker, args=(i, progress_queue, model_params,))
+        processes.append(proc)
+        proc.start()
+
+    while any([proc.is_alive() for proc in processes]):
+        pid, status = progress_queue.get()
+        if status:
+            progress_bars[pid].update()
+        if status == 9:
+            processes[pid].join()
 
 if __name__ == '__main__':
+    mp.set_start_method('spawn')
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--logdir', type=str, required=True)
     parser.add_argument('--params_file', type=str, required=True)
@@ -46,7 +92,10 @@ if __name__ == '__main__':
             'ship',
             'truck'
         ]
-        logname, logpath = generate_logname(model_params)
+        logname = generate_logname(model_params)
+        logpath = './' + model_params['logdir'] + '/' + logname
+        model_params['logname'] = logname
         model_params['logpath'] = logpath
         model_params['seed'] = arguments.seed
+        model_params['epochs'] = arguments.epochs
 
