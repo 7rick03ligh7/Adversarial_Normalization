b5026d9b7689b2678f8600a659a08d3a6001d9a6
7rick03ligh7 <7rick03ligh7@gmail.com>
2020-07-28 18:03:10 +0900

    [ADD] main debug

--- src/scripts/main.py
+++ src/scripts/main.py
@@ -18,9 +18,9 @@ from tqdm import tqdm
 
 
 def worker(pid, queue, model_params):
-    if os.path.exists(model_params['logpath']):
-        shutil.rmtree(model_params['logpath'])
-    os.makedirs(model_params['logpath'], exist_ok=True)
+    # if os.path.exists(model_params['logpath']):
+    #     shutil.rmtree(model_params['logpath'])
+    # os.makedirs(model_params['logpath'], exist_ok=True)
 
     model = VGGLike(model_params)
     tb_logger = pl_loggers.TensorBoardLogger(
@@ -49,7 +49,7 @@ def worker(pid, queue, model_params):
         )
     trainer.fit(model)
 
-def main(models_params):
+def main_parallel(models_params):
     progress_bars = []
     processes = []
     progress_queue = mp.Queue()
@@ -66,6 +66,35 @@ def main(models_params):
         if status == 9:
             processes[pid].join()
 
+def main_debug(model_params):
+    model_params = models_params[0]
+    tb_logger = pl_loggers.TensorBoardLogger(
+        model_params['logdir'],
+        name=model_params['logname'],
+        version=''
+        )
+    os.makedirs(model_params['logpath']+'/checkpoints/best/', exist_ok=True)
+    checkpoint_callback = ModelCheckpoint(
+        filepath=model_params['logpath']+'/checkpoints/best/',
+        save_top_k=1,
+        verbose=False,
+        monitor='val_acc',
+        mode='max',
+        prefix=''
+    )
+    trainer = Trainer(
+        logger=tb_logger,
+        checkpoint_callback=checkpoint_callback,
+        max_epochs=100,
+        num_sanity_val_steps=0,
+        weights_summary=None,
+        progress_bar_refresh_rate=0,
+        track_grad_norm=1,
+        gpus=[os.environ['CUDA_VISIBLE_DEVICES']]
+        )
+    trainer.fit(model)
+
+
 if __name__ == '__main__':
     mp.set_start_method('spawn')
 
@@ -74,6 +103,7 @@ if __name__ == '__main__':
     parser.add_argument('--params_file', type=str, required=True)
     parser.add_argument('--epochs', type=int, required=True)
     parser.add_argument('--seed', type=int, default=42, required=False)
+    parser.add_argument('--debug', type=bool, default=False)
     arguments = parser.parse_args()
 
     with open(arguments.params_file, 'r') as f:
@@ -98,4 +128,9 @@ if __name__ == '__main__':
         model_params['logpath'] = logpath
         model_params['seed'] = arguments.seed
         model_params['epochs'] = arguments.epochs
+
+    if arguments.debug:
+        main
+    else:
+        main(models_params)
 
