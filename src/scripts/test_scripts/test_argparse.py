bb42bb5eb1ed9123e40c9d04e97068499094c276
7rick03ligh7 <7rick03ligh7@gmail.com>
2020-07-30 05:46:00 +0900

    little fixes

--- src/scripts/test_scripts/test_argparse.py
+++ src/scripts/test_scripts/test_argparse.py
@@ -3,8 +3,8 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--a', type=str, required=True)
 parser.add_argument('--b', type=str, required=True)
-parser.add_argument('--c', type=int, required=True)
+parser.add_argument('-c', type=bool, default=False, const=True, nargs='?')
 parser.add_argument('--d', type=int, default=42, required=False)
 arguments = parser.parse_args()
 
-print(arguments.d)
+print(arguments.c)
